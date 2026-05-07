77360b7a4d8d2c7dbea4bd8a6c30783688cbcc7e
MoemengMoe <2019103537@qq.com>
2026-05-07 14:51:36 +0800

    fix: correct RTC display position and screen refresh region
    Fix wrong RTC time rendering position and incorrect screen refresh area.
    TODO: 1. Improve refresh rate with framebuffer caching
          2. Fix unresponsive button and temperature sensor interrupts
    
    修复RTC时间显示位置错误与屏幕刷新区域偏移问题。
    TODO: 1. 使用帧缓冲缓存提升刷新速度
          2. 解决按键和温度传感器中断无响应
    
    Original: 修复 fix: 1.修正了时间显示错误与刷新位置错误的问题 TODO：1.提升刷新速度 使用缓存刷新 2.解决中断按钮和温度传感器无响应

--- Core/Src/rtc_app.c
+++ Core/Src/rtc_app.c
@@ -5,6 +5,30 @@
 #include "rtc.h"
 #include<stdio.h>
 
+static HAL_StatusTypeDef rtc_fill_date_time(RTC_TimeTypeDef *sTime, RTC_DateTypeDef *sDate,
+                                            uint16_t year, uint8_t month, uint8_t day,
+                                            uint8_t weekday, uint8_t hour, uint8_t minute, uint8_t second)
+{
+    if (year < 2000 || year > 2099 || month < 1 || month > 12 || day < 1 || day > 31 ||
+        hour > 23 || minute > 59 || second > 59)
+    {
+        return HAL_ERROR;
+    }
+
+    sTime->Hours = hour;
+    sTime->Minutes = minute;
+    sTime->Seconds = second;
+    sTime->DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+    sTime->StoreOperation = RTC_STOREOPERATION_RESET;
+
+    sDate->Year = year - 2000;
+    sDate->Month = month;
+    sDate->Date = day;
+    sDate->WeekDay = (weekday >= RTC_WEEKDAY_MONDAY && weekday <= RTC_WEEKDAY_SUNDAY) ? weekday : RTC_WEEKDAY_MONDAY;
+
+    return HAL_OK;
+}
+
 void RTC_DATA_GET(RTC_TIME_DATA* data)
 {
     RTC_TimeTypeDef sTime = {0};
@@ -27,14 +51,11 @@ HAL_StatusTypeDef RTC_DATA_SET(uint16_t year, uint8_t month, uint8_t day, uint8_
     RTC_TimeTypeDef sTime={0};
     RTC_DateTypeDef sDate={0};
 
-    sTime.Hours=hour;
-    sTime.Minutes=minute;
-    sTime.Seconds=second;
+    if (rtc_fill_date_time(&sTime, &sDate, year, month, day, weekday, hour, minute, second) != HAL_OK)
+    {
+        return HAL_ERROR;
+    }
 
-    sDate.Year=year;
-    sDate.Month=month;
-    sDate.Date=day;
-    sDate.WeekDay=weekday;
     if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK)
     {
         return HAL_ERROR;
@@ -52,14 +73,12 @@ HAL_StatusTypeDef RTC_DATA_SET_BY_STRUCT(RTC_TIME_DATA* data)
 {
     RTC_TimeTypeDef sTime={0};
     RTC_DateTypeDef sDate={0};
-    sTime.Hours=data->hour;
-    sTime.Minutes=data->minute;
-    sTime.Seconds=data->second;
 
-    sDate.Year=data->year;
-    sDate.Month=data->month;
-    sDate.Date=data->day;
-    sDate.WeekDay=data->weekday;
+    if (rtc_fill_date_time(&sTime, &sDate, data->year, data->month, data->day, data->weekday,
+                           data->hour, data->minute, data->second) != HAL_OK)
+    {
+        return HAL_ERROR;
+    }
 
     if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK)
     {
